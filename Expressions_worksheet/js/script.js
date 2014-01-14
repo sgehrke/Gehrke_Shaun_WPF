50f25736699305f6238d6f1565853a8dc0f2e4fc
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-14 09:27:54 -0600

    Slice of Pie part 1
    Solving the second problem for the expessions_worksheet

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,5 +1,4 @@
 //Shaun Gehrke, 01-13-14, Expressions_worksheet
-<<<<<<< HEAD
 
 //Dog Years
 var actualAge = 5;//This variable can change with any given for Sparky's age 5 is just an example
@@ -7,5 +6,11 @@ const dogYears = 7;//This will never change cause a dog ages 7 times faster than
 var sparkyDogAge = (actualAge * dogYears);//An expression using variables to calculate Sparky's age in human years
 //print out the result
 console.log("Sparky is " + actualAge + " human years old which is " + sparkyDogAge  + " in dog years.");
-=======
->>>>>>> parent of 2ca3ef3... Solving the first problem
+
+//Slice of Pie part 1
+var pizzaSliceEach = 10;
+var numberStudents = 50;
+var numberPizzas = 30;
+var totalSlices = (pizzaSliceEach * numberPizzas);
+var result = (totalSlices / numberStudents);
+console.log("Each person ate " + result + " slices of pizza at the party");
