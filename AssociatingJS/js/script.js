793a029c995565a2f43fe73047a6d362343fd62c
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-13 22:45:22 -0600

    I am testing to see if I can commit right from Coda2. This is a test to see what happens and I may roll back.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,34 @@
-alert("Hello World!");
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//average is the sum of  the items devided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+//console.log(perimeter);
+
+var orangeBins = [234,567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+//console.log(total);
+
+var a = 6;
+var b = 7;
+var result = a + b;
+//console.log(result);
+
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+
+var phoneNo = "(" + String(areaCode) + ")" +  String(firstPart) + "-" + String(secPart);
+//console.log(phoneNo);
+
+var a  = 3;
+a = a + 4;
+console.log(a);
