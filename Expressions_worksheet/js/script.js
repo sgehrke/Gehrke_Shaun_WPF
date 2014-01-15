2283b47619c4f085f4c92b47f96b0ecb2b33cd54
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-14 19:42:20 -0600

    Finished comments and cloned git to Coda 2
    Added comments to the expressions_worksheet and cloned git to Coda 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,8 +2,8 @@
 
 //Dog Years
 var actualAge = 5;//This variable can change with any given for Sparky's age 5 is just an example
-const dogYears = 7;//This will never change cause a dog ages 7 times faster than a human
-var sparkyDogAge = (actualAge * dogYears);//An expression using variables to calculate Sparky's age in human years
+const DOGYEARS = 7;//This will never change cause a dog ages 7 times faster than a human
+var sparkyDogAge = (actualAge * DOGYEARS);//An expression using variables to calculate Sparky's age in human years
 //print out the result
 console.log("Sparky is " + actualAge + " human years old which is " + sparkyDogAge  + " in dog years.");
 
