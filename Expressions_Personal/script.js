307ae009159a80824bc45abdb28432889ab12ed8
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-15 21:22:24 -0600

    Added comment for picsPerDay
    Commented the what the line picsPerDay will do

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -3,4 +3,4 @@ var monthsPrego = prompt("Congratulations on your pregnancy and welcome to your 
 var monthsRemain = 9 - monthsPrego;//This line will calculate the remaining months of the pregnancy and be used later in an expression
 var targetPics = prompt("How many pictures are you aiming to take for the entire project?");////This will prompt the user to answer how many pictures they want to end up with
 var timesPerWeek = prompt("How many times per week are you taking pictures?");////This will prompt the user to answer how many times per week they will be having a session
-var picsPerDay = prompt("How many pictures are you taking per session?");
+var picsPerDay = prompt("How many pictures are you taking per session?");//This will prompt the user to enter how many pics per day they will be taking to be used later in a formula
