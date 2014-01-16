b04e87eeaf670ae3883e8e9937452a93c2b44ee7
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-16 08:48:02 -0600

    Added name and date
    Forgot to add my name and date

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -1,3 +1,4 @@
+//Shaun Gehrke, 01-15-14
 //Expressions_Personal - This Javascipt file will run a calculator to determine the amount of pctures in a pregnancy time lapse.
 var monthsPrego = prompt("Congratulations on your pregnancy and welcome to your time lapse journey!\nHow many months along are you?");//Intro statement and start a prompt of questions to be stored in variables
 var monthsRemain = 9 - monthsPrego;//This line will calculate the remaining months of the pregnancy and be used later in an expression
