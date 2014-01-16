74cefb454477764ea0a2b66aad1dfdb00d2c2868
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-15 21:18:10 -0600

    Added var timesPerWeek
    //This will prompt the user to answer how many times per week they will
    be having a session

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -2,3 +2,4 @@
 var monthsPrego = prompt("Congratulations on your pregnancy and welcome to your time lapse journey!\nHow many months along are you?");//Intro statement and start a prompt of questions to be stored in variables
 var monthsRemain = 9 - monthsPrego;//This line will calculate the remaining months of the pregnancy and be used later in an expression
 var targetPics = prompt("How many pictures are you aiming to take for the entire project?");////This will prompt the user to answer how many pictures they want to end up with
+var timesPerWeek = prompt("How many times per week are you taking pictures?");
