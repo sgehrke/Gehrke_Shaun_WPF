b0e3a18d4978f2b28ac19f49cff4bd5d3bb9aace
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-15 21:12:06 -0600

    Added var monthsRemain
    This line will calculate the remaining months of the pregnancy and be
    used later in an expression

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -1,2 +1,3 @@
 //Expressions_Personal - This Javascipt file will run a calculator to determine the amount of pctures in a pregnancy time lapse.
 var monthsPrego = prompt("Congratulations on your pregnancy and welcome to your time lapse journey!\nHow many months along are you?");//Intro statement and start a prompt of questions to be stored in variables
+var monthsRemain = 9 - monthsPrego;
