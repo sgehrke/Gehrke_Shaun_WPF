350ced02c07166e4dacd8be35ea31f467c0183d2
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-15 21:27:39 -0600

    Added var picsPerMonth
    //This will multiply the entered amount for pictures per day by 4

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -4,3 +4,4 @@ var monthsRemain = 9 - monthsPrego;//This line will calculate the remaining mont
 var targetPics = prompt("How many pictures are you aiming to take for the entire project?");////This will prompt the user to answer how many pictures they want to end up with
 var timesPerWeek = prompt("How many times per week are you taking pictures?");////This will prompt the user to answer how many times per week they will be having a session
 var picsPerDay = prompt("How many pictures are you taking per session?");//This will prompt the user to enter how many pics per day they will be taking to be used later in a formula
+var picsPerMonth = picsPerDay * 4;
