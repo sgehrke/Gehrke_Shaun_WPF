aa9b63ae30f95a005f5727f9aa8970bb0bcb6c35
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:35:58 -0600

    added array 2 condition
    // if the user returns a word this will make it a 0

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -18,3 +18,4 @@ console.log(packageArray[1]);//This will print the dj total part to the console
 packageArray[2] = prompt("To put that elegant touch on your wedding we suggest adding accent lighting also known as up-lighting, to your venue. One light will cover a 20 foot area.\nHow many lights do you require?");//This will prompt the user asking how many light they require and store in array 2
 //console.log(packageArray[2]);//This will print to console the users amount entered
 if (isNaN(packageArray[2])) { //This is a fail safe in case the user puts in a word
+	packageArray[2] = 0;// if the user returns a word this will make it a 0
