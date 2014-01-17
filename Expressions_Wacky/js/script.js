01b8e0caacd8330daf4243744bd0c05f139c2323
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 20:56:18 -0600

    Added var moneyYear
    the calculation that figures out how much money saved per year with
    users amount

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,11 @@
 //Shaun Gehrke, 01-16-14, Expressions_Wacky
-alert("Who wants to be a Millionaire!\nThis calculator will let you know when that's going to happen!");//This is the intro statement
+alert("Who wants to retire a Millionaire!\nThis calculator will let you know when that's going to happen!");//This is the intro statement
 var retireAge = prompt("Let's start with what age you would like to retire?");//this will gather their ideal retirment age
 console.log(retireAge);//Prints to the console the prompt response
+var yearBorn = prompt(retireAge + ", That's a great goal!\nNow, let's see how realistic it is.\nWhat year where you born?");//Prompts the user to share their birth year
+var age = 2014 - yearBorn;//calculates their age
+console.log(age);//prints their age to the console
+var moneyWeek = prompt("How much money do you plan on putting aside per week for your million dollar retirement fund?");
+var moneyYear = moneyWeek * 4 * 12;
+
+
