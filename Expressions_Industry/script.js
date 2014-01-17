b0859ca4b1a8ec4f54f36e96164146b996a0786c
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:11:49 -0600

    Added comments to the constants
    Added three comments to the constants

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,9 +1,9 @@
 //Shaun Gehrke, 01-15-14, Expressions_Indusrty
 //This will calculate an instant quote for my DJ service
 alert("Congratulation on your upcoming wedding and thank you for contacting The New Dance Machine DJ service!\nYou will now be asked a sequence of questions so we can custom build a DJ Package for your wedding.");//This is a welcome alert explaining what the javascript will run
-const BOOTHPRICE = 175;
-const DJRATE = 150; 
-const LIGHTRATE = 30;
+const BOOTHPRICE = 175;//This is a constant that will be used in the total formula
+const DJRATE = 150; //This is a constant that will be used in the total formula
+const LIGHTRATE = 30;//This is a constant that will be used in the total formula
 
 
 
