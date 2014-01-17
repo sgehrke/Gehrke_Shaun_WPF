640bd109b6a6c9f4dcd3fcd7b4a1319e6d79508a
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:50:16 -0600

    Final commit for Indusrty
    This is the last commit for Expressions_industry

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -6,7 +6,7 @@ const DJRATE = 150; //This is a constant that will be used in the total formula
 const LIGHTRATE = 30;//This is a constant that will be used in the total formula
 
 var weddingMonth = prompt("What month are you getting married?");//This line will start a string that will be the entire wedding date
-var weddingDate = prompt("What is the date of the wedding");//This line will prompt the user for their date…This should be a number
+var weddingDate = prompt("What is the date of the wedding?");//This line will prompt the user for their date…This should be a number
 var weddingYear = prompt("What year is your wedding?");//This will prompt the user to add the year of their wedding
 var weddingDateString =  weddingMonth + " " + weddingDate + ", " + weddingYear;//This var will concatenate the above variables into one string
 console.log(weddingDateString);//This will print to the console with the what the wedding date is together
