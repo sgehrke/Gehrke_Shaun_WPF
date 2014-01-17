c7de225e399d4cac7d61fbb52f6b07f39edd254b
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:37:22 -0600

    Added an alert for array 2
    ;//Here the will be alerted that a mistake has occured and that there
    will be no lights added to their package

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -19,3 +19,5 @@ packageArray[2] = prompt("To put that elegant touch on your wedding we suggest a
 //console.log(packageArray[2]);//This will print to console the users amount entered
 if (isNaN(packageArray[2])) { //This is a fail safe in case the user puts in a word
 	packageArray[2] = 0;// if the user returns a word this will make it a 0
+	alert("You have entered an invalid number, there will be no lights added to your package.");//Here the will be alerted that a mistake has occured and that there will be no lights added to their package
+
