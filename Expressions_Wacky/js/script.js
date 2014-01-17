756806b0935e18c12f70e7d2539024b620ff6950
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 22:02:23 -0600

    Added round goal
    This is the roun goal

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -15,10 +15,17 @@ var roundYear = Math.round(yearsToMillion);
 console.log(roundYear);
 var goal = yearsToMillion + age - retireAge;
 var roundGoal = Math.round(goal);
+console.log(roundGoal);
+
 if (roundGoal <= 0) {
-	alert("this")
+	var roundGoal = Math.abs(roundGoal);
+	console.log(roundGoal);
+	var goaReached = retireAge - roundGoal;
+	alert("Congratulations, you will be a millionaire" + roundGoal + " before your goal")
+} else {
+	alert("That");
 }
-console.log(goal);
+
 
 
 
