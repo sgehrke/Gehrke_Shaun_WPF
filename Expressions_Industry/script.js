f473328004df0b0fc7165ff93b39b4253fe6e5ab
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:31:56 -0600

    Added array 2
    //This will prompt the user asking how many light they require and
    store in array 2

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -15,4 +15,4 @@ var packageArray = [];//This line creates a container for the documents variable
 packageArray[0] = prompt("How many hours of Dj service do you require?");//This array will store in the 0 index and be used in a later calculation
 packageArray[1] = packageArray[0] * DJRATE;//This array will multiply hours needed with the rate
 console.log(packageArray[1]);//This will print the dj total part to the console
-
+packageArray[2] = prompt("To put that elegant touch on your wedding we suggest adding accent lighting also known as up-lighting, to your venue. One light will cover a 20 foot area.\nHow many lights do you require?");//This will prompt the user asking how many light they require and store in array 2
