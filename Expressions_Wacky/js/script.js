bf64bbb2415ca990196a2c7dc2c2d426b7997cd3
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 20:35:14 -0600

    Added var retireAge
    This is a prompt for when the user would like to retire

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,2 +1,4 @@
 //Shaun Gehrke, 01-16-14, Expressions_Wacky
-alert("Who wants to be a Millionaire!\nThis calculator will let you know when that's going to happen!")//This is the intro statement
+alert("Who wants to be a Millionaire!\nThis calculator will let you know when that's going to happen!");//This is the intro statement
+var retireAge = prompt("Let's start with what age you would like to retire?");//this will gather their ideal retirment age
+console.log(retireAge);//Prints to the console the prompt response
