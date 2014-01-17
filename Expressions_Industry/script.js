3792417c57c28ee5143d33c5617c323c2a88b388
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 19:08:12 -0600

    Added intro comment
    //This is a welcome alert explaining what the javascript will run

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,5 +1,7 @@
 //Shaun Gehrke, 01-15-14, Expressions_Indusrty
 //This will calculate an instant quote for my DJ service
-alert("Congratulation on your upcoming wedding and thank you for contacting The New Dance Machine DJ service!\nYou will now be asked a sequence of questions so we can custom build a DJ Package for your wedding.")
+alert("Congratulation on your upcoming wedding and thank you for contacting The New Dance Machine DJ service!\nYou will now be asked a sequence of questions so we can custom build a DJ Package for your wedding.");//This is a welcome alert explaining what the javascript will run
+
+
 
 
