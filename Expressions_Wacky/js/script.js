ec09757e9eb6cb63d5d8a0183dd956b0f11f0ee4
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-01-16 22:13:18 -0600

    Added money saving comment
    //This line prompts the user to see how much money they will be saving

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,7 +5,7 @@ console.log(retireAge);//Prints to the console the prompt response
 var yearBorn = prompt(retireAge + ", That's a great goal!\nNow, let's see how realistic it is.\nWhat year where you born?");//Prompts the user to share their birth year
 var age = 2014 - yearBorn;//calculates their age
 console.log(age);//prints their age to the console
-var moneyWeek = prompt("How much money do you plan on putting aside per week for your million dollar retirement fund?");
+var moneyWeek = prompt("How much money do you plan on putting aside per week for your million dollar retirement fund?");//This line prompts the user to see how much money they will be saving
 var moneyYear = moneyWeek * 4 * 12;
 console.log(moneyYear);
 var monthsToMillion = 1000000 / moneyYear;
