e035fb0e46ce5abead4a7fcfb3ad76303c6ef999
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-20 21:42:48 -0600

    Added the celsius to Fahrenheit converter
    Added the first var to the equation

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,4 +4,8 @@
 var competitorWeight = 275;
 (competitorWeight > 250) ? console.log("The competitor qualifies for the heavyweight division.") : console.log("The comprtitor needs to gain some weight!");
 
-var degrees = prompt("Are you contverting to Celsius or Fehrenheit? Please use C for Celsius and F for Fehrenheit.")
+
+//Celsius to Fahrenheit converter
+var degrees = prompt("Are you contverting to Celsius or Fehrenheit? Please use C for Celsius and F for Fehrenheit.");
+console.log(degrees);
+
