1fa850123eabb07df0f608684978857431c98d27
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-23 11:19:50 -0600

    Added while statement
    validation of the first prompt. Also added parse int to make the string
    a number

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,2 +1,43 @@
 //Shaun Gehrke, 01-22-14, Conditionals_Personal
+//The Beats calculator
+//This calculator will assume the average bpm of te genre of music your spinning, to estimate the number musical beats your brain listens to a day.
+var bpmArray = [120 , 140, 70, 160, 90];
+var genre = prompt("So what's on the platter today?\nLayman's terms:\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");
+genre = parseInt(genre);	
+while ((genre === "") || (isNaN(genre)) || (genre <1 || genre >5)) {
+	genre = prompt("You need to enter the number that corrisponds with your genre.\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");	
+}
+genre = parseInt(genre);
+
+
+var hoursPlayed = prompt("How many hours did you spin for?");
+
+if (genre >0 || genre <6){
+	switch (genre){
+		case 1: 
+			genre = bpmArray[0]
+			break;
+		case 2: 
+			genre = bpmArray[1]
+			break;
+		case 3: 
+			genre = bpmArray[2]
+			break;
+		case 4: 
+			genre = bpmArray[3]
+			break;
+		case 5: 
+			genre = bpmArray[4]
+			break;		
+	}
+}
+console.log(genre)
+
+
+
+var beatsPerHour = (genre * 60);
+var bpmToday = (hoursPlayed * beatsPerHour);
+
+(bpmToday < 15000) ? alert("You are well on your way to becoming a local club DJ. Your spinning " + bpmToday + " beats per day, and that is right where you need to be to practice and still enjoy it!") : alert("You are a needle junkie! You are spinning " + bpmToday + " beats per day. At this rate you will burn yourself out and live a sheltered life. Find a balance soon before it is to late!");
+
 
