57d65a20452207c26b6425a5dc12f1e4ee0d72df
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-23 11:50:25 -0600

    Added comments for the first part of personal
    Described what the first part of the document will do

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,13 +1,13 @@
 //Shaun Gehrke, 01-22-14, Conditionals_Personal
 //The Beats calculator
 //This calculator will assume the average bpm of te genre of music your spinning, to estimate the number musical beats your brain listens to a day.
-var bpmArray = [120 , 140, 70, 160, 90];
-var genre = prompt("So what's on the platter today?\nLayman's terms:\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");
-genre = parseInt(genre);	
+var bpmArray = [120 , 140, 70, 160, 90];//This array includes the genres bpm
+var genre = prompt("So what's on the platter today?\nLayman's terms:\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");//This will prompt the user for the type of music they are using
+genre = parseInt(genre);//This make their responce into a number
 while ((genre === "") || (isNaN(genre)) || (genre <1 || genre >5)) {
-	genre = prompt("You need to enter the number that corrisponds with your genre.\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");	
+	genre = prompt("You need to enter the number that corrisponds with your genre.\nWhat genre of music will you be spinning today?\n1) House\n2) Trance\n3) Dubstep\n4) Drum-n-Bass\n5) Hip-Hop");//This is a failsafe for if the user doesn't enter a valid number or leaves the prompt blank
 }
-genre = parseInt(genre);
+genre = parseInt(genre);//This will turn the string into a number if they entered invalid info
 
 
 var hoursPlayed = prompt("How many hours did you spin for?");
@@ -39,5 +39,5 @@ var beatsPerHour = (genre * 60);
 var bpmToday = (hoursPlayed * beatsPerHour);
 
 (bpmToday < 15000) ? alert("You are well on your way to becoming a local club DJ. Your spinning " + bpmToday + " beats per day, and that is right where you need to be to practice and still enjoy it!") : alert("You are a needle junkie! You are spinning " + bpmToday + " beats per day. At this rate you will burn yourself out and live a sheltered life. Find a balance soon before it is to late!");
-
+console.log(bpmToday + " beats per day!")
 
