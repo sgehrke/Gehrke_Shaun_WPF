525a38cd6958374f009cac296885216ff1332709
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-29 13:15:01 -0600

    Prompted first question
    setup gender variable with a prompt and validated with at while

--- Functions_ Personal/js/script.js
+++ Functions_ Personal/js/script.js
@@ -1,4 +1,19 @@
 //Shaun Gehrke, 01-29-14, Functions_Personal
-alert("Test")
+//This generator will randomly suggest baby names
+alert("So, your having trouble coming up with names for your new baby!\nThis name generator is here help with some suggestions!");
+var gender = prompt("Are you having a boy or a girl?");
+gender = gender.toLowerCase();
+console.log(gender);
+
+while (gender === "" || (!isNaN(gender)) || gender != "boy" && gender != "girl"){
+	gender = prompt("Please enter either BOY or GIRL into the prompt box!");
+	gender = gender.toLowerCase();
+}
+
+console.log(gender);
 
 
+var extBoyName = extBoyNameArray(Liam, Emmett, Cabe, Mackson, Adalius, Grey, Lachlan, Rush);
+var extGirlName = extGirlNameArray(Betson, Neri, Kyra, Isabelline, Legacy, Tempe, Camber, Atticus);
+var commonBoyName = commonBoyNameArray(Bruce, Dennis, Harry, Jesse, Ken, Kurt, Luke, Roy);
+var commonGirlName = commonGirlNameArray(Anna, Bridget, Elenor, Faith, Jane, Lucy, Rose, Stella);
