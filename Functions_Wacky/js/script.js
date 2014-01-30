6ed36e541f7682eceec719b7621e063e966dfeb5
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-30 16:07:09 -0600

    Added the alert and law size prompt
    Added the intro to let the user know what the calculator will do and
    prompted for their lawn size

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,16 +1,21 @@
 //Shaun Gehrke, 01-30-14, Functions_Wacky
 //This will calculate how many goats it would take to mow your lawn for you
+alert("Ever get sick of spending countless hours cutting your lawn?\nEvery wish there was an easier way?\nWhy not let goats take care of it!");
 
-
+var lawnSize =  prompt("First, we will need to know how big your yard is.\nSo, How many square feet is the lawn you want mowed?");
+	while (lawnSize === "" || (isNaN(lawnSize))){
+	lawnSize = prompt("You have entered an invalid response.\nHow many square feet is your lawn?");
+	}
 
 function lawnGoats(yardSize, hours){
 	var goatsEatPerSqFt = 10;
 	var feetPerHour = yardSize / hours
 	var goats = feetPerHour / goatsEatPerSqFt;
 	return goats;
-	
 }
 
-var goats = lawnGoats(1000, 2);
-
+var goats = lawnGoats(lawnSize, timeNeeded);
+var goats = parseInt(goats);
 console.log(goats);
+
+
