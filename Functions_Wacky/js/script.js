66648f3a0049abc6653e120838698687a5251af5
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-30 16:08:09 -0600

    Added timeNeeded var
    this var will ask the user how much time they are giving the goats

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,6 +6,15 @@ var lawnSize =  prompt("First, we will need to know how big your yard is.\nSo, H
 	while (lawnSize === "" || (isNaN(lawnSize))){
 	lawnSize = prompt("You have entered an invalid response.\nHow many square feet is your lawn?");
 	}
+var timeNeeded = prompt("Now, how many hours will you give the goats to mow down your yummy grass?");
+	while (timeNeeded === "" || (isNaN(timeNeeded))){
+	timeNeeded = prompt("That is not a valid entry.\nHow many hours will you give the goats to finish the job?");
+	}
+if (timeNeeded <= 2) {
+	timeNeeded = prompt("Wow! Remember these are goats eating your lawn.\nAt that rate you would be responsible for more dead goats than days of the year!\nCan you please enter a higher number of hours for the goats sake! ");
+		
+}
+
 
 function lawnGoats(yardSize, hours){
 	var goatsEatPerSqFt = 10;
