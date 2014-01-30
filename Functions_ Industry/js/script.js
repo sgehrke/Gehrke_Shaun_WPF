da9c82d83eb54e363f5e9a184e6cfcdbfa789296
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-30 09:01:07 -0600

    Linked the js with the html
    Made an alert intro message to see if the files were connected

--- Functions_ Industry/js/script.js
+++ Functions_ Industry/js/script.js
@@ -1,4 +1,4 @@
 //Shaun Gehrke, 01-29-14, Functions_Indusrty
 //This calculator will estimate the cost of your new web site
 
-
+alert("So your ready to join the the modern world and get a website made for your company!\nThis estimator will give you an idea of how much that will cost you.");
