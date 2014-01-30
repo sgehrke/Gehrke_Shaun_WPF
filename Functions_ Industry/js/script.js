c86fd5a6586ea8c52a40dac2c5c326e10c059aa4
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-30 12:12:26 -0600

    Start Array and company prompt
    Started the Array of gathered info..includin type of site

--- Functions_ Industry/js/script.js
+++ Functions_ Industry/js/script.js
@@ -5,6 +5,25 @@ const EMAILCOST = 30;
 //Thiswill prompt the user as to what tis javascript does
 alert("So your ready to join the the modern world and get a website made for your company!\nThis estimator will give you an idea of how much that will cost you.");
 
+var myArray = [];
+
+var compName = prompt("What is the name of your company?");
+console.log(compName);
+
+var siteType = prompt("What type of site do you require?\n1) Static\n2) Dynamic\n3) Template based");
+console.log(siteType);
+
+while (siteType === "" || (isNaN(siteType)) || siteType <1 || siteType >3){
+	siteType = prompt("Please enter a valid number.\nWhat type of site do you require?\n1) Static\n2) Dynamic\n3) Template based");
+}
+
+if (siteType === "1"){
+	siteType = 50;
+} else if (siteType === "2"){
+	siteType = 100;
+} else {
+	siteType = 75;
+}
 
 
 
