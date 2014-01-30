bc377f21ecdf7b218e0cf293d4403c6347768087
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-30 15:27:01 -0600

    Added a function to Wacky
    Made the calculation function to Wacky. and set a return

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,16 @@
-//Shaun Gehrke, 01-29-14, Functions_
+//Shaun Gehrke, 01-30-14, Functions_Wacky
+//This will calculate how many goats it would take to mow your lawn for you
 
 
+
+function lawnGoats(yardSize, hours){
+	var goatsEatPerSqFt = 10;
+	var feetPerHour = yardSize / hours
+	var goats = feetPerHour / goatsEatPerSqFt;
+	return goats;
+	
+}
+
+var goats = lawnGoats(1000, 2);
+
+console.log(goats);
