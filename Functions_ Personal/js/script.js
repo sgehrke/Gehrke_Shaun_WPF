254930f7d152c72d0476b4aa731e43da49f5905a
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-01-29 23:23:41 -0600

    Fixed Array
    Fixed the array to use [ ]  and to contain a string.

--- Functions_ Personal/js/script.js
+++ Functions_ Personal/js/script.js
@@ -1,4 +1,3 @@
-/*
 //Shaun Gehrke, 01-29-14, Functions_Personal
 //This generator will randomly suggest baby names
 alert("So, your having trouble coming up with names for your new baby!\nThis name generator is here to offer you some suggestions!");
@@ -13,35 +12,34 @@ while (gender === "" || (!isNaN(gender)) || gender != "boy" && gender != "girl")
 console.log(gender);
 
 if (gender === "boy"){
-	var boyNameType = prompt("Looks like there will be some ruckus in your future!\nWhat type of name do you prefer?\n1) Common\n2) Extravagant");
-		while (boyNameType === "" || (isNaN(boyNameType)) || boyNameType <1 || boyNameType >2) {
-			boyNameType = prompt("You need to enter a number.\nWhat type of name do you prefer?\n1) Common\n2) Extravagant ");
+	var nameType = prompt("Looks like there will be some ruckus in your future!\nWhat type of name do you prefer?\n1) Common\n2) Extravagant");
+		while (nameType === "" || (isNaN(nameType)) || nameType <1 || nameType >2){
+		nameType = prompt("You need to enter a number.\nWhat type of name do you prefer?\n1) Common\n2) Extravagant ");
+			console.log(nameType);
 		}
 	
 } else if (gender === "girl") {
-	var girlNameType = prompt("Looks like you will be hosting some tea parties in your future!\nWhat type of name would you prefer for your little princess?\n1) Common\n2) Extravagent");
-		while (girlNameType === "" || (isNaN(girlNameType)) || girlNameType <1 || girlNameType >2) {
-			girlNameType = prompt("You need to enter a number.\nWhat type of name do you prefer?\n1) Common\n2) Extravagant ");			
+	var nameType = prompt("Looks like you will be hosting some tea parties in your future!\nWhat type of name would you prefer for your little princess?\n1) Common\n2) Extravagent");
+		while (nameType === "" || (isNaN(nameType)) || nameType <1 || nameType >2){
+		nameType = prompt("You need to enter a number.\nWhat type of name do you prefer?\n1) Common\n2) Extravagant ");
+			console.log(nameType);			
 		}
 }
-*/
-
-var randomNameNum = numgen();
-
-function numgen() {
-	var randomNum = Math.floor((Math.random() * 8) + 1);
-console.log(randomNum)	
-	return;
-}
 
 
+var genderNum = gender + nameType;
+console.log(genderNum);
 
-/*
-var extBoyName = extBoyNameArray(Liam, Emmett, Cabe, Mackson, Adalius, Grey, Lachlan, Rush);
-var extGirlName = extGirlNameArray(Betson, Neri, Kyra, Isabelline, Legacy, Tempe, Camber, Atticus);
-var commonBoyName = commonBoyNameArray(Bruce, Dennis, Harry, Jesse, Ken, Kurt, Luke, Roy);
-var commonGirlName = commonGirlNameArray(Anna, Bridget, Elenor, Faith, Jane, Lucy, Rose, Stella);
+var extBoyName = ["Liam", "Emmett", "Cabe", "Mackson", "Adalius", "Grey", "Lachlan", "Rush"];
+var extGirlName = ["Betson", "Neri", "Kyra", "Isabelline", "Legacy", "Tempe", "Camber", "Atticus"];
+var commonBoyName = ["Bruce", "Dennis", "Harry", "Jesse", "Ken", "Kurt", "Luke", "Roy"];
+var commonGirlName = ["Anna", "Bridget", "Elenor", "Faith", "Jane", "Lucy", "Rose", "Stella"];
 
-*/
+
+var randomNameNum = numgen(extBoyName);
+console.log(randomNameNum);
+
+
+
 
 
